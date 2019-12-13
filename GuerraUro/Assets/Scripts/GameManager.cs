ff58a32ab78836aa34919407bd13fbf582550924
swatstar98 <robertitomedusiyo@gmail.com>
2019-12-13 14:01:55 +0100

    Added GM

---
+++ GuerraUro/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] orangeSpawnersArray;
+    public GameObject[] blueSpawnersArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orangeSpawnersArray = GameObject.FindGameObjectsWithTag("OrangeSpawners");
+        blueSpawnersArray = GameObject.FindGameObjectsWithTag("BlueSpawners");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
